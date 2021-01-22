aa358cea8baac3ab2c2b114ff13051fb98cd757a
pu6372689 <pu6372689@gmail.com>
2021-01-22 14:40:46 +0800

    commit

--- crawl/start-crawl.sh
+++
@@ -1,8 +0,0 @@
-d=$(date '+%Y%m')
-
-mkdir -p $d
-
-docker run -v /tmp/study/crawl/crawl.py:/tmp/crawl/crawl.py \
--v /tmp/study/crawl/$d:/tmp/crawl/$d \
---rm test python /tmp/crawl/crawl.py
-
--- dockercompose/work/get-requirements/venvsave.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-. /home/jovyan/test/bin/activate
-#activate venv
-pip freeze > /home/jovyan/work/python-env/requirements.txt
-#get python env
-
